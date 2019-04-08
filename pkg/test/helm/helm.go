9872b20de3ae5e7e37790586e08f211bceb185a0
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2019-04-08 11:44:16 -0700

    Simplify the Template()

--- pkg/test/helm/helm.go
+++ pkg/test/helm/helm.go
@@ -16,10 +16,10 @@ package helm
 
 import (
 	"fmt"
+	"unicode"
+
 	"istio.io/istio/pkg/test/scopes"
 	"istio.io/istio/pkg/test/shell"
-	"strings"
-	"unicode"
 )
 
 // Init calls "helm init"
@@ -40,81 +40,24 @@ func Init(homeDir string, clientOnly bool) error {
 }
 
 func Template(homeDir, template, name, namespace string, valuesFile string, values map[string]string) (string, error) {
-	// Apply the overrides for the values file.
-	// valuesString := ""
-
-	//var args []string
-	//args=append(args, fmt.Sprintf("--home %s template %s --name %s", homeDir, template, namespace))
-
-
-	valuesFileString := ""
+	p := []string{"helm", "--home", homeDir, "template", template, "--name", name, "--namespace", namespace}
 	if valuesFile != "" {
-		valuesFileString = fmt.Sprintf("--values %s", valuesFile)
+		p = append(p, "--values")
+		p = append(p, valuesFile)
 	}
 
-	//	args=append(args, fmt.Sprintf("%s", valuesFileString))
-
-
-	//for k, v := range values {
-	//	//valuesString += fmt.Sprintf(" --set %s=%s", k, v)
-	//	if containSpaces(v) {
-	//		// Quote the string value so that a string "part1 part2" is not
-	//		// interpreted as " --set key=part1 part2".
-	//					valuesString += fmt.Sprintf(" --set %s=\"%s\"", k, v)
-	//		//args=append(args, fmt.Sprintf(" --set %s=\"%s\"", k, v))
-	//	} else {
-	//					valuesString += fmt.Sprintf(" --set %s=%s", k, v)
-	//		//args=append(args, fmt.Sprintf(" --set %s=%s", k, v))
-	//	}
-	//}
-
-	//args := fmt.Sprintf("--home %s template %s --name %s --namespace %s %s %s",
-	//	homeDir, template, name, namespace, valuesFileString, valuesString)
-	// Unlike Execute(), ExecuteArgs() does not split the command line by
-	// the space character, thereby not having the problem of an argument
-	// with space chars is splitted into multiple arguments.
-	// E.g., --set "part1 part2" will not be treated as --set part1 part2; instead,
-	// "part1 part2" is treated as one single argument.
-//	out, err := shell.ExecuteArgs(nil, "helm", args[0:]...)
-
-	//out, err := shell.Execute("helm --home %s template %s --name %s --namespace %s %s %s",
-	//	homeDir, template, name, namespace, valuesFileString, valuesString)
-
-	str := fmt.Sprintf("helm --home %s template %s --name %s --namespace %s %s",
-		homeDir, template, name, namespace, valuesFileString)
-
-	parts := strings.Split(str, " ")
-
-	var p []string
-	for i := 0; i < len(parts); i++ {
-		if parts[i] != "" {
-			p = append(p, parts[i])
-		}
-	}
-
+	// Override the values in the helm value file.
 	for k, v := range values {
-		//valuesString += fmt.Sprintf(" --set %s=%s", k, v)
-		if k=="" {
+		if k == "" {
 			continue
 		}
 		p = append(p, "--set")
 		p = append(p, fmt.Sprintf("%s=%s", k, v))
-		//if containSpaces(v) {
-		//	// Quote the string value so that a string "part1 part2" is not
-		//	// interpreted as " --set key=part1 part2".
-		//	p = append(p, fmt.Sprintf("%s=\"%s\"", k, v))
-		//	//args=append(args, fmt.Sprintf(" --set %s=\"%s\"", k, v))
-		//} else {
-		//	p = append(p, fmt.Sprintf("%s=%s", k, v))
-		//	//args=append(args, fmt.Sprintf(" --set %s=%s", k, v))
-		//}
 	}
 	out, err := shell.ExecuteArgs(nil, "helm", p[1:]...)
-
 	if err != nil {
 		scopes.Framework.Errorf("helm template: %v, out:%q", err, out)
 	}
-
 
 	return out, err
 }
