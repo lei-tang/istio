3a1d569dcb89861bde4da75e89aaade915415ec5
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2019-10-16 18:20:01 -0700

    Update the run-demo script

--- demo-dns-webhooks-10-25-2019/run-demo.sh
+++ demo-dns-webhooks-10-25-2019/run-demo.sh
@@ -23,9 +23,45 @@ kubectl exec -it $GALLEY_POD -n istio-system -- cat /var/run/secrets/kubernetes.
 kubectl get secret dns.istio-galley-service-account -n istio-system -o json | jq -r '.data["cert-chain.pem"]' | base64 --decode | openssl x509 -in - -text -noout
 kubectl get secret dns.istio-sidecar-injector-service-account -n istio-system -o json | jq -r '.data["cert-chain.pem"]' | base64 --decode | openssl x509 -in - -text -noout
 
-
 # Check that there is no webhook configurations before running istioctl to enable webhook configurations.
 kubectl get validatingwebhookconfiguration
 kubectl get mutatingwebhookconfiguration
 
+# Generate the MutatingWebhookConfiguration and ValidatingWebhookConfiguration
+helm template \
+    --name=istio \
+    --namespace=istio-system \
+    --values install/kubernetes/helm/istio/example-values/values-istio-dns-cert.yaml \
+    install/kubernetes/helm/istio > istio-webhook-config.yaml
+
+# Save the MutatingWebhookConfiguration and ValidatingWebhookConfiguration into
+# sidecar-injector-webhook.yaml and galley-webhook.yaml.
+
+# Enable webhook configurations through istioctl:
+/usr/local/google/home/leitang/go/out/linux_amd64/release/istioctl experimental post-install webhook enable --validation --webhook-secret dns.istio-galley-service-account \
+    --namespace istio-system --validation-path galley-webhook.yaml \
+    --injection-path sidecar-injector-webhook.yaml
+
+# Check the Sidecar Injector webhook is working:
+kubectl create namespace test-injection; kubectl label namespaces test-injection istio-injection=enabled
+kubectl run --generator=run-pod/v1 --image=nginx nginx-app --port=80 -n test-injection
+kubectl get pod -n test-injection
+
+# Check the validation webhook is working:
+cat <<EOF > ./invalid-gateway.yaml
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: invalid-gateway
+spec:
+  selector:
+    # DO NOT CHANGE THESE LABELS
+    # The ingressgateway is defined in install/kubernetes/helm/istio/values.yaml
+    # with these labels
+    istio: ingressgateway
+EOF
+
+kubectl create namespace test-validation
+# The invalid gateway yaml should be rejected by the validation webhook.
+kubectl apply -f invalid-gateway.yaml -n test-validation
 
